4d61c4b10ffce2f7a7798b634b1db596a4b3cc9a
koloff <koloff.anton@gmail.com>
2016-02-29 21:19:11 +0200

    added more logic in the course edit area

--- src/components/new-path/Box.vue
+++ src/components/new-path/Box.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="container-box">
-    <div class="content-box">
-      {{index}}
+    <div class="container-box">
+      <div class="content-box">
+        {{boxPosition}}
+      </div>
+        <img class="arrow" src="../../assets/img/arrow.png" alt="next">
     </div>
-    <div class="arrow-box">
-      <img class="arrow" src="../../assets/img/arrow.png" alt="next">
-    </div>
-  </div>
 </template>
 
 <script>
   import boxOptions from './box-options';
-  
+
   export default {
     name: 'Box',
     props: ['resource', 'index'],
+    data() {
+      return {
+        boxPosition: []
+      };
+    },
     ready() {
 
       $('.container-box')
@@ -29,17 +32,10 @@
           width: boxOptions.boxWidth,
           height: boxOptions.boxHeight
         });
-
-      $('.arrow')
-        .css({
-          width: boxOptions.arrowWidth,
-          height: boxOptions.arrowHeight
-        });
     },
 
     events: {
       'boxes-positions-calculated': function(positions) {
-        console.log('calculatedd');
         this.boxPosition = positions[this.index];
         this.calculateStyles();
       }
@@ -48,10 +44,58 @@
     methods: {
       calculateStyles() {
         let pos = this.boxPosition;
-        console.log(pos);
+        let $container = $(this.$el);
 
-        if (pos.indexOf('first') > -1 && pos.indexOf('odd') > -1) {
-          // first odd
+        if (pos.indexOf('first') > -1 && pos.indexOf('even') > -1) {
+          // every first odd box should have left margin in order for the boxes to be aligned
+          $container.css({
+            marginLeft: `${boxOptions.horizontalArrowSizes.width}px`
+          });
+        }
+
+        if (pos.indexOf('odd') > -1) {
+          console.log($container);
+          $container.find('.content-box')
+            .css({
+              float: 'right'
+            });
+        }
+
+
+        // set arrow styles
+        let $arrow = $container.find('.arrow');
+
+        if (pos.indexOf('last') > -1) {
+          //noinspection JSSuspiciousNameCombination
+          $arrow
+            .css({
+              display: 'inline-block',
+              transform: 'rotate(90deg)',
+              transformOrigin: '0 0',
+              // swap width and height because of the rotate
+              width: boxOptions.verticalArrowSizes.height,
+              height: boxOptions.verticalArrowSizes.width,
+              marginTop: `${boxOptions.boxHeight + boxOptions.verticalArrowSizes.marginStart}px`,
+              marginBottom: `10px`
+            });
+
+          if (pos.indexOf('even') > -1) {
+            $arrow
+              .css({
+                marginLeft: `-${boxOptions.boxWidth / 2 - boxOptions.verticalArrowSizes.width / 2}px`
+              })
+          } else {
+            $arrow
+              .css({
+//                marginLeft: `${boxOptions.boxWidth / 2 - boxOptions.verticalArrowSizes.width / 2 + boxOptions.horizontalArrowSizes.width}px`
+              })
+          }
+        } else {
+          $container.find('.arrow')
+            .css({
+              width: boxOptions.horizontalArrowSizes.width,
+              height: boxOptions.horizontalArrowSizes.height
+            })
         }
       }
     }
@@ -65,9 +109,14 @@
     padding: 0;
     display: inline-block;
     background: red;
+    border: 1px solid purple;
   }
 
   .content-box {
     background: green;
+  }
+
+  .arrow {
+    position: absolute;
   }
 </style>
--- src/components/new-path/box-options.js
+++ src/components/new-path/box-options.js
@@ -1,24 +1,41 @@
 // size of the resource information box
-const boxWidth = 100;
+const boxWidth = 170;
 const boxHeight = 100;
-// size of the arrows between the resources
-const arrowWidth = 20;
-const arrowHeight = 20;
-// margins in the two ends of the arrow: [start margin] ---> [end margin]
-const arrowStartMargin = 5;
-const arrowEndMargin = 5;
 
-let containerBoxWidth = boxWidth + arrowWidth + arrowStartMargin + arrowEndMargin;
-let containerBoxHeight = boxHeight + arrowHeight + arrowStartMargin + arrowEndMargin;
+// horizontal arrow sizes
+const horizontalArrowSizes = {
+  width: 40,
+  height: 30,
+  marginStart: 5,
+  marginEnd: 5
+};
+
+// vertical arrow sizes
+const verticalArrowSizes = {
+  width: 20,
+  height: 60,
+  marginStart: 20,
+  marginEnd: 5
+};
+
+let containerBoxWidth =
+  boxWidth +
+  horizontalArrowSizes.width +
+  horizontalArrowSizes.marginStart +
+  horizontalArrowSizes.marginEnd;
+
+let containerBoxHeight =
+  boxHeight +
+  verticalArrowSizes.height +
+  verticalArrowSizes.marginStart +
+  verticalArrowSizes.marginEnd;
 
 
 export default {
   boxWidth,
   boxHeight,
-  arrowWidth,
-  arrowHeight,
-  arrowStartMargin,
-  arrowEndMargin,
+  horizontalArrowSizes,
+  verticalArrowSizes,
   containerBoxWidth,
   containerBoxHeight
 }
